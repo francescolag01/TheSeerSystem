59f6e86af4c0c6539a242f1c64601f062ecd0a6c
francescolag01 <92961820+francescolag01@users.noreply.github.com>
2024-04-01 20:47:25 -0400

    Add files via upload
    All of the code is contained in TheSeerSystem.java, for now. The PNG is a placeholder image that the application uses. jSerialComm is a serial communications library, so we can read from the Arudino.

--- TheSeerSystem.java
+++ TheSeerSystem.java
@@ -1,27 +1,105 @@
 
 
-import javax.swing.SwingUtilities;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.BorderFactory;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+//I downloaded this library from https://fazecast.github.io/jSerialComm/ , to use the Arduinos serial interface
+import com.fazecast.jSerialComm.*;
 
 public class TheSeerSystem {
+	public static JFrame f;
+	public static Image planeImage;
    
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
+            	//initializes the GUI
                 createAndShowGUI();
+                //load and store the plane image
+                planeImage = loadPlane();
+                
+                //establish serial connection to the Arudino
+                //we create the comport obj and open the arudino port
+                SerialPort comPort = SerialPort.getCommPort("COM4"); // or whatever port we end up using
+                comPort.openPort();
+         
+                try {
+                while (true)
+                {
+                	 //wait for the serial interrupt
+                	while(comPort.bytesAvailable() == 0) {
+
+                	     
+                	}
+                	//read the serial port
+                	//TODO: Determine how we want to send the message from the arduino, and how to process it in this application 
+             	    byte[] readBuffer = new byte[comPort.bytesAvailable()];
+                	int numRead = comPort.readBytes(readBuffer, readBuffer.length);
+                	System.out.println("Read " + numRead + " bytes.");
+                		
+             	   //determine the current conditions of the runway based on read data
+             	 
+             	   //TODO:What is the main functionality of the software?
+	                	//Popups stating arrivals, departures, near misses, and collisions
+	                	//the runways changing color based on if there is a plane or not
+	                	//draw a plane icon on the screen, relative to the physical plane's location on the runway
+                		//near the plane icon, draw its speed to the screen
+                	
+                	//TODO: What features can we add for polish?
+                		//draw text to label useful information
+             	   		//change direction of plane if it goes backwards
+                		//add a logo
+                		//
+             	   //redraw the screen
+
+                  
+                }
+             } catch (Exception e) {
+            	 e.printStackTrace();
+            	 }
+             comPort.closePort();
+            	  
+                
                
-            }
-        });
+            
+            }});
     }
+    
+
+public void CloseCallPopUp() {
+	
+	JOptionPane.showMessageDialog(TheSeerSystem.f,"DANGER: AIRCRAFT ARE LESS THAN 90000 FEET APART.");
+	
+}
+public void CrashPopUp() {
+	
+	JOptionPane.showMessageDialog(TheSeerSystem.f,"OH NO: AIRCRAFT HAVE COLLIDED ON RUNWAY 20 AND 23.");
+}
+
+public void ArrivalPopUp() {
+	
+	JOptionPane.showMessageDialog(TheSeerSystem.f,"Plane has landed on runway 12");
+}
+
+public void DeparturePopUp() {
+	
+	JOptionPane.showMessageDialog(TheSeerSystem.f,"Plane has taken off from runway 13");
+}
+
 
     private static void createAndShowGUI() {
     
-        JFrame f = new JFrame("The Seer System");
+        f = new JFrame("The Seer System");
         f.setSize(new Dimension(1000,1000));
         f.setPreferredSize(new Dimension(1000,1000));
         f.setLocationRelativeTo(null);
@@ -32,7 +110,26 @@ public class TheSeerSystem {
         f.setVisible(true);
     }
     
-   public Color getColor(String sensorData) {
+    
+public static Image loadPlane() {
+//loads the plane image and puts it in an Image object, to display on the screen
+Image img = null;
+
+//loads the png
+URL path = TheSeerSystem.class.getResource("imgbin_airplane-png.png");
+    	try {
+    	img = ImageIO.read(new File(path.toString()));
+    	}catch( Exception e) {
+    		System.out.println("Could not load airplane image.");
+    		e.printStackTrace();
+    		
+    	}
+    	
+return img;
+    }
+
+    
+public Color getColor(String sensorData) {
 	   
 	   Color C = Color.GREEN;
 	   
@@ -74,11 +171,17 @@ class drawPanel extends JPanel {
         g.fillRect(475,100,50,800); //sensor 1
         
         //draw planes based on location
-        
+        /*if(sensor 1 or 2 detected)}
+    
+        g.drawImage(getPlaneImg(), string sensordata)
+    	}
+    */
     }
 }
-	
+
+
 
 
 	
+
 	
